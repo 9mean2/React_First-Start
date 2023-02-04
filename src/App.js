367f6af985d6d60686703a9f261bea0d389070ff
9mean2 <>
2023-02-05 07:24:33 +0900

    feat: state를 이용한 예제 문제 풀기, 아이디 패스워드

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("오일남할아버지");
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log("id", id);
+  console.log("password", password);
+
+  //id 필드가 변경될 경우
+  const onIdChaneHandler = (event) => {
+    setId(event.target.value);
+  };
+
+  //password 필드가 변경될 경우
+  const onPasswordChaneHandler = (event) => {
+    setPassword(event.target.value);
+  };
 
   return (
     <div>
-      {name}
-      <br />
+      <div>
+        아이디 : <input type="text" value={id} onChange={onIdChaneHandler} />
+      </div>
+      <div>
+        비밀번호 :{" "}
+        <input
+          type="password"
+          value={password}
+          onChange={onPasswordChaneHandler}
+        />
+      </div>
       <button
-        onClick={function () {
-          setName("숨겨진 반전 ㄷㄷ");
+        onClick={() => {
+          alert(
+            `고객님이 입력하신 아이디는 ${id}이며, 패스워드는 ${password}입니다.`
+          );
+          setId("");
+          setPassword("");
         }}
       >
-        클릭
+        로그인
       </button>
     </div>
   );
