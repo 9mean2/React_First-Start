93b5dfede49a8cfc15f5ba912206ab7c332b6b81
9mean2 <>
2023-02-04 13:31:59 +0900

    added color

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 
-function Son() {
-  return <div>전 아들인데요</div>;
-}
-
-function Mother() {
-  return <Son />;
-}
-
-function GrandFather() {
-  return <Mother />;
-}
-
 function App() {
-  return <GrandFather />;
+  const number = 1;
+
+  const pTagStyle = {
+    color: "red",
+  };
+
+  return (
+    <div>
+      <p style={pTagStyle}>안녕하세요 리액트입니다.</p>
+      <p style={pTagStyle}>
+        {number > 10 ? number + "은 10 보다 크다" : number + "는 10보다 작다"}
+      </p>
+    </div>
+  );
 }
 
 export default App;
